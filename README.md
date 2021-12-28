c05ff3018b8110055a7b43f6acb2ef344f79d90c
github.com/jtzjtz/hys_gen <github.com/jtzjtz/hys_gen@163.com>
2021-12-28 21:11:19 +0800

    测试

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-go get -insecure github.com/jtzjtz/kit
+#代码自动生成
+### 代码自动生成两层代码，并上传到git供consumer和provider引入对应包使用
+####1.client：消费者，引入包即可使用
+####2.server：grpc服务提供者，引入包即可使用
+
+
+![系统截图](系统截图.png)
--- git_push.sh
+++ git_push.sh
@@ -5,7 +5,7 @@ echo "开始push<br>"
 
 git add -A
 git commit  -m "$2"
-git push https://$4 main
+git push https://$4 origin:master
 git tag -a $3 -m "$2"
 git push  https://$4  --tags
 echo "push 完成"
