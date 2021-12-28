7f5a7af8553c0f0134abf8011b36c787145b43f3
github.com/jtzjtz/hys_gen <github.com/jtzjtz/hys_gen@163.com>
2021-12-28 20:04:34 +0800

    asdf

--- git_pull.sh
+++ git_pull.sh
@@ -4,6 +4,7 @@ rm -rf "$5"
 
 echo "开始拉取代码<br>"
 git clone http://$2:$3@$4.git $5
+#git clone http://$2:$3@$4.git $5  //有用户名密码使用
 echo "拉取代码完成<br>"
 git config --global user.email "$2@163.com"
 git config --global user.name "$2"
