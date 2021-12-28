69786c3835c59e70456593105427ab49a8e918a1
github.com/jtzjtz/hys_gen <github.com/jtzjtz/hys_gen@163.com>
2021-12-28 21:30:54 +0800

    测试

--- git_push.sh
+++ git_push.sh
@@ -5,7 +5,7 @@ echo "开始push<br>"
 
 git add -A
 git commit  -m "$2"
-git push https://$4 origin:master
 git tag -a $3 -m "$2"
-git push  https://$4  --tags
+
+git push https://$4 master:master --tags
 echo "push 完成"
